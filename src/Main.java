cb64805ff7de42a127bbd6a307f019e06920279a
Lenar Khafizov <lenarkh@mail.ru>
2022-12-04 11:04:57 +0300

    Задача 1 к д/з Лямбда-выражения и функциональные интерфейсы выполнена.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 public class Main {
     public static void main(String[] args) {
@@ -10,9 +11,19 @@ public class Main {
         people.add((new Person("Игорь", "Мари Соли Боярт", 15)));
         people.add((new Person("Алексей", "Шувалов", 18)));
         people.add((new Person("Антон", "Андре Себастьян Жардье", 18)));
-        Collections.sort(people, new PersonSurnameComparator(1));
-        System.out.println(people);
-        Collections.sort(people, new PersonSurnameComparator(2));
+        Comparator <Person> comparator = (Person o1, Person o2) -> {
+            String[] surname1 = o1.getSurname().split("\\P{IsAlphabetic}+");
+            String[] surname2 = o2.getSurname().split("\\P{IsAlphabetic}+");
+            int surnameLength1 = Math.min(surname1.length, 2);
+            int surnameLength2 = Math.min(surname2.length, 2);
+            int surnameCompare = Integer.compare(surnameLength2, surnameLength1);
+            if (surnameCompare == 0) {
+                return Integer.compare(o2.getAge(), o1.getAge());
+            } else {
+                return surnameCompare;
+            }
+        };
+        Collections.sort(people, comparator);
         System.out.println(people);
 
     }
