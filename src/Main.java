450b046f77eb9e9920971ea8b557490785a094dc
Lenar Khafizov <lenarkh@mail.ru>
2022-12-09 23:00:37 +0300

    Задача 2 к д/з Лямбда-выражения и функциональные интерфейсы выполнена.

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,11 @@ public class Main {
         List<Person> people = new ArrayList<>();
         people.add((new Person("Иван", "Максимов", 16)));
         people.add((new Person("Виктор", "Апполонов-Краснов", 17)));
-        people.add((new Person("Игорь", "Мари Соли Боярт", 15)));
+        people.add((new Person("Игорь", "Мари Соли Боярт", 19)));
         people.add((new Person("Алексей", "Шувалов", 18)));
         people.add((new Person("Антон", "Андре Себастьян Жардье", 18)));
-        Collections.sort(people, new PersonSurnameComparator(1));
-        System.out.println(people);
+        people.removeIf(person -> person.getAge() < 18);
         Collections.sort(people, new PersonSurnameComparator(2));
         System.out.println(people);
-
     }
 }
